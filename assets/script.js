05fccb9231ea37410beaf2ec1883726f00b18506
Ashley Lerma <lerma.scout@gmail.com>
2020-05-15 16:22:48 -0500

    fleshed out for loop and defined variables

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,27 @@
 // Displays the current date and time
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
 
-// Based on current time apply the appropriate class (.present, .past, .future) to timeblock
-/* 
-    1. Run a function as soon as the page loads - jQuery(document).ready(function(){ }
+// Gets current hours our of 24
+var d = new Date();
+let currentHour = d.getHours();
 
-    inside the function - 
-
-    4. Assign each text area a value correspong to the timeBlockHour
-    */
-
+//  Run a function as soon as the page loads - jQuery(document).ready(function(){ }
 jQuery(document).ready(function () {
-  // 2. pull value of current hour and asign to a variable (let currentHour = ??)
-  let currentHour;
-
-  //   time block values to check against
+  //   time block values to check against text area values
   let timeBlockHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
   //  check current time against each time block
   for (let i = 0; i < timeBlockHour.length; i++) {
     timeBlock = timeBlockHour[i];
     if (currentHour > timeBlock) {
+      $("").addClass("past");
       //  apply .past to textarea with value i
     } else if ((currentHour = timeBlock)) {
       // apply .present to textarea with value i
+      $("").addClass("present");
     } else if (currentHour < timeBlock) {
       // apply .future to textarea with value i
+      $("").addClass("future");
     }
   }
 });
