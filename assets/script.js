e962b5acc9cf17bd15fedca73456ad7cf2ea680c
Ashley Lerma <lerma.scout@gmail.com>
2020-05-15 15:59:07 -0500

    created basic for loop

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,29 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
 
     inside the function - 
 
-    2. pull value of current hour and asign to a variable (let currentHour = ??)
-    3. Create local variable for time block hour values in military time (let timeBlockHour = [9-17])
     4. Assign each text area a value correspong to the timeBlockHour
-    5. Create a for loop to check each time block (let i = 0, i < timeBlockHour.length, i ++)
-    6. if (currentHour > timeBlockHour[i]){apply .past to textarea with value i}
-    7. if (currentHour = timeBlockHour[i]){apply .present to textarea with value i}
-    8. if (currentHour < timeBlockHour[i]){apply .future to textarea with value i}
     */
+
+jQuery(document).ready(function () {
+  // 2. pull value of current hour and asign to a variable (let currentHour = ??)
+  let currentHour;
+
+  //   time block values to check against
+  let timeBlockHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+  //  check current time against each time block
+  for (let i = 0; i < timeBlockHour.length; i++) {
+    timeBlock = timeBlockHour[i];
+    if (currentHour > timeBlock) {
+      //  apply .past to textarea with value i
+    } else if ((currentHour = timeBlock)) {
+      // apply .present to textarea with value i
+    } else if (currentHour < timeBlock) {
+      // apply .future to textarea with value i
+    }
+  }
+});
+
 // Save button for each row that commits input text to local storage (on click event)
 // Local storage is displayed in the text area of corresponding time block
-// make text area read only for past time blocks
+// make text area read only for .past
